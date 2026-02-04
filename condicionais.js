c93fc8ada5dda6c7425264ab3f1992a38100de7a
nayaracalenzo <queiroz.14@hotmail.com>
2026-02-04 10:12:22 -0300

    condicionais if else

--- condicionais.js
+++ condicionais.js
@@ -15,24 +15,43 @@
 // mais de uma condição -> else if
 
 //exemplo: situação de aluno
-let nota1 = Number(prompt("digite a primeira nota"))
-let nota2 = Number(prompt("digite a segunda nota"))
-let nota3 = Number(prompt("digite a terceira nota"))
+// let nota1 = Number(prompt("digite a primeira nota"))
+// let nota2 = Number(prompt("digite a segunda nota"))
+// let nota3 = Number(prompt("digite a terceira nota"))
 
-let media = ((nota1 + nota2 + nota3) / 3).toFixed(1)
+// let media = ((nota1 + nota2 + nota3) / 3).toFixed(1)
 
-if (media >= 7) {
-  alert(`Aluno aprovado ${media}`)
-} else if (media < 7 && media >= 5) {
-  alert(`Aluno em recuperação ${media}`)
-} else {
-  alert(`Aluno reprovado ${media}`)
-}
+// if (media >= 7) {
+//   alert(`Aluno aprovado ${media}`)
+// } else if (media < 7 && media >= 5) {
+//   alert(`Aluno em recuperação ${media}`)
+// } else {
+//   alert(`Aluno reprovado ${media}`)
+// }
 
 
 //Pergunte ao usuário se ele possui habilitação e é maior de 18 anos
+//const idade = Number(prompt("Digite sua idade"))
+const idade = prompt("É maior de 18 anos? Responda: sim ou não").toLowerCase()
 
 //caso ele seja menor de 18 anos -> ele não pode tirar habilitação
+let possuiHabilitacao
+if (idade === "não") {
+  alert("Não pode tirar habilitação")
+} else if (idade === "sim") {
+  possuiHabilitacao = prompt("Possui habilitação? Responda: sim ou não").toLowerCase()
+} else {
+  alert("Resposta inválida")
+}
+
+if (possuiHabilitacao === "não") {
+  alert("ele pode tentar a prova do detran")
+} else if (possuiHabilitacao === "sim") {
+  alert("ele pode dirigir")
+} else if (!!possuiHabilitacao) {
+  alert("Resposta inválida")
+}
 
 //caso ele tenha 18 anos ou mais e não possui habilitação -> ele pode tentar a prova do detran
 //caso ele tenha 18 ou mais e possui habilitação -> ele pode dirigir
+
